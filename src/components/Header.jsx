fa69795a79e62516a4fa0c70d6d9b2e910f40613
aloshvp <aloshvp12@gmail.com>
2025-09-18 23:12:25 +0530

    fetch data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
         <header className='header'>
-            <Link to="/">Home</Link>
-            <Link to="/posts">Posts</Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/posts">Posts</NavLink>
         </header >
     )
 }
--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const PostList = () => {
+
+    const [postData, setPostData] = useState([]);
+
+    useEffect(() => {
+        fetchPostData();
+    })
+
+    const fetchPostData = async () => {
+        const data = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+        const json = await data.json();
+        setPostData(json);
+    }
+
     return (
-        <div>PostList</div>
+        <>
+            <div>PostList</div>
+            <div>
+                {postData?.map((post) => (
+                    <React.Fragment key={post?.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </React.Fragment>
+                ))
+                }
+            </div >
+        </>
     )
 }
 
