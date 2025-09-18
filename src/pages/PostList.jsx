cb1d992d305212943212d291d4db52042b49454e
aloshvp <aloshvp12@gmail.com>
2025-09-18 23:18:26 +0530

    loader for post items

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -3,16 +3,26 @@ import React, { useEffect, useState } from 'react'
 const PostList = () => {
 
     const [postData, setPostData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         fetchPostData();
-    })
+    }, [])
 
     const fetchPostData = async () => {
-        const data = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
-        const json = await data.json();
-        setPostData(json);
+        setLoading(true);
+        try {
+            const data = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+            const json = await data.json();
+            setPostData(json);
+        }
+        catch (err) {
+            console.log("error" + err)
+        }
+        setLoading(false);
     }
+
+    if (loading) return <p>Loading</p>
 
     return (
         <>
