f70ddc9632a91b6ac71f6d7d55d665739fb480c9
aloshvp <aloshvp12@gmail.com>
2025-09-19 23:45:31 +0530

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import './App.css'
 import AppLayout from './layout/appLayout'
 import Home from './pages/Home'
 import PostList from './pages/PostList'
 import { PostLoader } from './pages/PostList'
 import PostComments from './pages/PostComments'
+import Header from './components/Header'
 
 const router = createBrowserRouter([
   {
@@ -28,11 +29,28 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-
-
   return (
     <RouterProvider router={router} />
   )
 }
+
+
+// Older way
+// function App() {
+//   return (
+//     <>
+//       {/* <AppLayout /> */}
+
+//       <BrowserRouter>
+//         <Header />
+//         <Routes>
+//           <Route path="/" element={<Home />} ></Route >
+//           <Route path="/posts" element={<PostList />} ></Route >
+//           <Route path="/posts/:postId" element={<PostComments />} ></Route >
+//         </Routes >
+//       </BrowserRouter >
+//     </>
+//   )
+// }
 
 export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Email:" + email);
+        console.log("Password:" + password);
+        if (email && password) {
+            navigate("/posts");
+        }
+
+    }
+
     return (
         <div>
-            Home
+            <form action="post" onSubmit={handleSubmit}>
+                <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                < input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
